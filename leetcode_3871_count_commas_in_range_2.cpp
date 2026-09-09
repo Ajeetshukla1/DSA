4ff1c7f0ac0303b07fa4973e8190f8f972bb5f6d
Ajeetshukla1 <ajeetshukla543@gmail.com>
2026-09-10 01:35:26 +0530

    day 5 daily problem count commas in range 2

---
+++ leetcode_3871_count_commas_in_range_2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long sum=0;
+        if(n-999>0) sum+=n-999;
+        if(n-999999>0) sum+=n-999999;
+        if(n-999999999>0) sum+=n-999999999;
+        if(n-999999999999>0) sum+=n-999999999999;
+        if(n-999999999999999>0) sum+=n-999999999999999;
+
+        return sum;    
+    }
+};
