a49c96bfae1453fb098b8381c1aaf70abaf92c2d
Ajeetshukla1 <ajeetshukla543@gmail.com>
2026-09-20 23:41:38 +0530

    day 14 weekly contest 9/20/2026

---
+++ leetcode_contests/weekly_9-20-2026/4057.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long countIntersectingIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end());
+        priority_queue<int, vector<int>, greater<int>> pq;
+        long long cnt = 0;
+        for (int i = 0; i < n; i++) {
+            while (!pq.empty() && pq.top() < intervals[i][0]) {
+                pq.pop();
+            }
+            cnt += pq.size();
+            pq.push(intervals[i][1]);
+        }
+        return cnt;
+    }
+};
